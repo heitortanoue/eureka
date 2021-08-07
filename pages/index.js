993fc6f96a1fabedf2581008f0b79e386a7b9fe9
Heitor Tanoue <68477006+totoi690@users.noreply.github.com>
2021-08-06 21:55:40 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,26 +4,24 @@ import TextBox from "../components/out/textbox"
 import UserFeedback from "../components/out/userfeedback"
 import Footer from "../components/out/footer"
 import Head from "next/head"
-import RedirectWhenLogged from "../utils/redirectWhenLogged"
 
 export default function Home() {
   return (
     <>
-      <RedirectWhenLogged/>
-      <Head>
-        <title>Eureka</title>
-      </Head>
-      <div className="font-body">
-        <div className="mb-32">
-          <Header/>
-          <Container>
-            <Main/>
-            <TextBox/>
-            <UserFeedback/>
-          </Container>
+    <Head>
+      <title>Eureka</title>
+    </Head>
+    <div className="font-body">
+      <div className="mb-32">
+        <Header/>
+        <div className={`font-body container mx-auto px-5 lg:px-10 flex flex-col gap-20 p-5 md:p-10`}>
+          <Main/>
+          <TextBox/>
+          <UserFeedback/>
         </div>
-        <Footer/>
       </div>
+      <Footer/>
+    </div>
     </>
   )
 }
