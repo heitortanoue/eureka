50df8166923ed3b33372225d9c768bcf08f94752
totoi690 <tozaomello@gmail.com>
2021-08-06 21:50:51 -0300

    versão mobile paginaPrincipal

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,6 @@ import Main from "/components/out/main"
 import TextBox from "../components/out/textbox"
 import UserFeedback from "../components/out/userfeedback"
 import Footer from "../components/out/footer"
-import Container from "../components/global/container"
 import Head from "next/head"
 
 export default function Home() {
@@ -15,11 +14,11 @@ export default function Home() {
     <div className="font-body">
       <div className="mb-32">
         <Header/>
-        <Container>
+        <div className={`font-body container mx-auto px-5 lg:px-10 flex flex-col gap-20 p-5 md:p-10`}>
           <Main/>
           <TextBox/>
           <UserFeedback/>
-        </Container>
+        </div>
       </div>
       <Footer/>
     </div>
